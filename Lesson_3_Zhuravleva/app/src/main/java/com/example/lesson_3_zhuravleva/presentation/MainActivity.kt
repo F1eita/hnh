7e663d141a6de18003b2db7e547a8bf9d0cde0c3
Valeriya Zhuravleva <113582969+F1eita@users.noreply.github.com>
2023-10-29 17:55:34 +0300

    lesson 3

---
+++ Lesson_3_Zhuravleva/app/src/main/java/com/example/lesson_3_zhuravleva/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.lesson_3_zhuravleva.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.lesson_3_zhuravleva.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
